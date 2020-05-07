e8367004b48f02701de98004e26e8a016b4e03ed
Cedric Joaquin <joaquin.cedric@gmail.com>
2020-05-07 01:07:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,7 @@
 def line(people_in_line)
-  current_line = []
+  current_line = "The line is currently: "
   if people_in_line > 0 
-    "The line is currently: "
+    counter = 1
+    while counter < people_in_line.size do
+      current_line += "#{counter}. #people"
   
