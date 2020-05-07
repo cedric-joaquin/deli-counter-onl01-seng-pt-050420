b706fb958d8b5d6d0ed33afe040a9979be0aa4c8
Cedric Joaquin <joaquin.cedric@gmail.com>
2020-05-07 01:09:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 def line(customers)
-  current_line = "The line is currently: "
+  current_line = "The line is currently:"
   if customers.size > 0 
     counter = 1
     while counter <= customers.size do
-      current_line += "#{counter}. #{customers[counter - 1]} "
+      current_line += " #{counter}. #{customers[counter - 1]}"
       counter += 1 
     end
     puts current_line
